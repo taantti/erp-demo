73985c738313c224c2d22befa95aa24e5f1e56d9
taantti <taan.kehitys@gmail.com>
2025-08-09 20:28:36 +0300

    Delete loginTime model.

--- src/modules/login/services/loginService.js
+++ src/modules/login/services/loginService.js
@@ -3,7 +3,6 @@ import jwt from 'jsonwebtoken';
 import config from './../../../config.js';
 import { User } from '../../../models/index.js';
 
-
 export const login = async (req, res) => {
     console.log("req.body", req.body);
     console.log("loginService.js: login(): ");
@@ -25,8 +24,11 @@ export const login = async (req, res) => {
 
         //return bcrypt.compareSync(password,  user.password);
         if(!bcrypt.compareSync(password,  user.password)) return false;
-
-        const token = jwt.sign({user_id: user._id}, config.JWT_SECRET_KEY, {
+        const payload = {
+            user_id: user._id
+        }
+        //const token = jwt.sign({user_id: user._id}, config.JWT_SECRET_KEY, {
+        const token = jwt.sign(payload, config.JWT_SECRET_KEY, {
            expiresIn:  config.JWT_TOKEN_EXPIRATION
         });
 
