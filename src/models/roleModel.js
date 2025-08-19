54cda1c875433ae4469f65eaa4af70343691e93e
taantti <taan.kehitys@gmail.com>
2025-08-19 20:06:14 +0300

    Update roleModel.js to support RBAC.

--- src/models/roleModel.js
+++ src/models/roleModel.js
@@ -1,8 +1,32 @@
-import mongoose from 'mongoose';
+import mongoose, { MongooseError } from 'mongoose';
+
+const featurePermissionSchema = new mongoose.Schema({
+    feature: { type: String, required: true, minlength: 3, maxlength: 30 },
+    access: { type: Boolean, required: true },
+    adminTenantOnly: { type: Boolean, required: true },
+    immutable: { type: Boolean, required: true },
+});
+
+const modulePermission = new mongoose.Schema({
+    module: { 
+        type: String, 
+        required: true, 
+        minlength: 3, 
+        maxlength: 60,
+        enum: [
+            'product', 
+            'role', 
+            'tenant', 
+            'user'
+        ]
+    },
+    features: [featurePermissionSchema]        
+});
 
 const roleSchema = new mongoose.Schema({
-    name: String,
-    role: String
+    name: { type: String, required: true, minlength: 3, maxlength: 30 },
+    role: { type: String, required: true, enum: ['OVERSEER', 'ADMIN', 'WRITER', 'READER']},
+    permissions: [modulePermission]
 });
 
 const Role = mongoose.model('Role', roleSchema);
