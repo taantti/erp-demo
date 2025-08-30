3b506dc1bc6acfa12df62c0935b855ff4f8caf46
taantti <taan.kehitys@gmail.com>
2025-08-30 15:12:10 +0300

    Update sanitizationMiddleware to use utility sanitization module.

--- src/middleware/sanitizationMiddleware.js
+++ src/middleware/sanitizationMiddleware.js
@@ -1,6 +1,4 @@
-import sanitizeHtml from "sanitize-html";
-import aux from "./../utils/auxiliary.js";
-import {sanitizeNestedJson}  from "./../utils/sanitization.js";
+import {sanitizeValue, sanitizeNestedJson}  from "./../utils/sanitization.js";
 
 /* 
 * Middleware to sanitize incoming requests
@@ -9,41 +7,30 @@ import {sanitizeNestedJson}  from "./../utils/sanitization.js";
 * @param {Function} next - The next middleware function.
 */
 const sanitizeRequest = (req, res, next) => {
-
-    aux.cLog("sanitizeRequest: PRE SANITATION: req.body = " + JSON.stringify(req.body, null, 2));
-
+    // Method and body check
     if (req.method != "GET" && req.method != "DELETE") {
-        aux.cLog("sanitizeRequest: req.method = " + req.method);
-        if (!req.body) return res.status(400).json({ error: 'Missing request body' });
+        if (!req.body) return res.status(400).json({ error: `Missing ${req.method} request body.` });
     }
 
     // Body sanitation
     req.body = sanitizeNestedJson(req.body);
-    aux.cLog("POST SANITATION: req.body = " + JSON.stringify(req.body, null, 2));
 
     // Authorization header sanitation.
-    if (req.headers && req.headers['authorization']) {
-        req.headers.authorization = sanitizeHtml(req.headers['authorization'], { // Sanitize HTML
-            allowedTags: [], // No HTML tags allowed
-            allowedAttributes: {} // No attributes allowed
-        }).trim(); // Remove leading/trailing whitespaces
-    }
-    
+    if (req.headers && req.headers['authorization']) req.headers.authorization = sanitizeValue(req.headers['authorization']);
+
     // Http query sanitation
     if (req.query) {
         for (const key in req.query) {
-            let value = req.query[key];
-            if (typeof value === 'string') {
-                value = value.trim();
-                value = sanitizeHtml(value, {
-                    allowedTags: [],
-                    allowedAttributes: {}
-                });
-                req.query[key] = value;
-            }
+           if (typeof req.query[key] === 'string') req.query[key] = sanitizeValue(req.query[key]);
         }
     }
-
+    
+    // Http params sanitation
+    if (req.params) {
+        for (const key in req.params) {
+            if (typeof req.params[key] === 'string') req.params[key] = sanitizeValue(req.params[key]);
+        }
+    }
     next();
 }
 
