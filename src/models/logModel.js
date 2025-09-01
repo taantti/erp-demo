691574e0ad10b21a5d3713dc59d9ceb860292e56
taantti <taan.kehitys@gmail.com>
2025-09-01 20:31:22 +0300

    Add Log model.

---
+++ src/models/logModel.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+import { logLevels, log } from  "../utils/logger.js";
+
+export const msgMinLength = 1;
+export const msgMaxLength = 1200;
+
+const LogSchema = new mongoose.Schema({
+    level: { type: String, required: true, enum: logLevels, default: 'INFO' },
+    message: { type: String, required: true, minlength: msgMinLength, maxlength: msgMaxLength },
+    date_time: { type: Date, default: Date.now },
+    tenant: { type: mongoose.Schema.Types.ObjectId, ref: 'Tenant', required: false },
+});
+
+export const Log = mongoose.model('Log', LogSchema);
+
+/* No implementation yet
+export const findLog = (req, allTenants = false) => {
+
+}
+
+export const findOneLog = (req, allTenants = false) => {
+      if(!allTenants && !req.user?.tenant) {
+        log('', '');
+        return false;
+    }  
+}
+
+export const findLogByIdAndDelete = (req, allTenants = false) => {
+        if(!allTenants && !req.user?.tenant) {
+        log('', '');
+        return false;
+    }
+}
+
+export const findLogById = (req, allTenants = false) => {
+        if(!allTenants && !req.user?.tenant) {
+        log('', '');
+        return false;
+    }
+}
+*/
