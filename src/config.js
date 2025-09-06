6f4db1a5b8354f03ded8d6442ea6fa3ca45608c0
taantti <taan.kehitys@gmail.com>
2025-09-06 19:37:58 +0300

    Update config.js with default values.

--- src/config.js
+++ src/config.js
@@ -4,22 +4,21 @@ dotenv.config();
 /* Configuration settings loaded from environment variables. */
 const config = {
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_HOST: process.env.DATABASE_HOST,
-    DATABASE_PORT: process.env.DATABASE_PORT,
-    DATABASE_NAME: process.env.DATABASE_NAME,
-    DATABASE_USERNAME: process.env.DATABASE_USERNAME,
-    DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
-    PORT: process.env.PORT,
-    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
-    JWT_PASSPHRASE: process.env.JWT_PASSPHRASE,
-    JWT_TOKEN_EXPIRATION: process.env.JWT_TOKEN_EXPIRATION,
-    BCRYPT_SALT_ROUNDS: process.env.BCRYPT_SALT_ROUNDS,
+    DATABASE_HOST: process.env.DATABASE_HOST || '',
+    DATABASE_PORT: process.env.DATABASE_PORT || '',
+    DATABASE_NAME: process.env.DATABASE_NAME || '',
+    DATABASE_USERNAME: process.env.DATABASE_USERNAME || '',
+    DATABASE_PASSWORD: process.env.DATABASE_PASSWORD || '',
+    PORT: process.env.PORT || '',
+    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY || '',
+    JWT_PASSPHRASE: process.env.JWT_PASSPHRASE || '',
+    JWT_TOKEN_EXPIRATION: process.env.JWT_TOKEN_EXPIRATION || '',
+    BCRYPT_SALT_ROUNDS: process.env.BCRYPT_SALT_ROUNDS || '',
     INIT: (process.env.INIT.toLowerCase() === "true"),
     INSTALL: (process.env.INSTALL.toLowerCase() === "true"),
     ADD: (process.env.ADD.toLowerCase() === "true"),
     LOG_LEVEL: process.env.LOG_LEVEL || 'INFO',
     LOG_TO_FILE: (process.env.LOG_TO_FILE.toLowerCase() === "true"),
-
 }
 
 export default config;
