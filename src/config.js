3cae2d82aaff41edd179136b43ed6aa4e16c8a14
taantti <taan.kehitys@gmail.com>
2025-09-07 19:59:36 +0300

    Small updates to config.js.

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ dotenv.config();
 /* Configuration settings loaded from environment variables. */
 const config = {
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_HOST: process.env.DATABASE_HOST || '',
+    DATABASE_HOST: process.env.DATABASE_HOST || 'localhost',
     DATABASE_PORT: process.env.DATABASE_PORT || '',
     DATABASE_NAME: process.env.DATABASE_NAME || '',
     DATABASE_USERNAME: process.env.DATABASE_USERNAME || '',
@@ -13,7 +13,7 @@ const config = {
     JWT_SECRET_KEY: process.env.JWT_SECRET_KEY || '',
     JWT_PASSPHRASE: process.env.JWT_PASSPHRASE || '',
     JWT_TOKEN_EXPIRATION: process.env.JWT_TOKEN_EXPIRATION || '',
-    BCRYPT_SALT_ROUNDS: process.env.BCRYPT_SALT_ROUNDS || '',
+    BCRYPT_SALT_ROUNDS: process.env.BCRYPT_SALT_ROUNDS || 10,
     INIT: (process.env.INIT.toLowerCase() === "true"),
     INSTALL: (process.env.INSTALL.toLowerCase() === "true"),
     ADD: (process.env.ADD.toLowerCase() === "true"),
